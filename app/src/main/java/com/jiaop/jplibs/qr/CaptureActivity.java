0f0e691dc17b74717031737f9199abe902d43adf
JiaoP <hijiaop@outlook.com>
2018-07-27 18:03:25 +0800

    Update
    增添二维码ZXing的封装及使用示例

---
+++ app/src/main/java/com/jiaop/jplibs/qr/CaptureActivity.java
@@ -0,0 +1,284 @@
+package com.jiaop.jplibs.qr;
+
+import android.animation.ObjectAnimator;
+import android.animation.ValueAnimator;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.graphics.Rect;
+import android.hardware.Camera;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.animation.LinearInterpolator;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+
+import com.google.zxing.Result;
+import com.jiaop.jplibs.R;
+import com.jiaop.libs.utils.UIUtil;
+import com.jiaop.libs.zxing.BeepManager;
+import com.jiaop.libs.zxing.CameraManager;
+import com.jiaop.libs.zxing.CaptureAct;
+import com.jiaop.libs.zxing.CaptureActivityHandler;
+import com.jiaop.libs.zxing.DecodeThread;
+import com.jiaop.libs.zxing.InactivityTimer;
+
+import java.io.IOException;
+import java.lang.reflect.Field;
+
+import butterknife.BindView;
+
+public class CaptureActivity extends CaptureAct implements SurfaceHolder.Callback {
+
+    @BindView(R.id.capture_preview)
+    SurfaceView scanPreview;
+    @BindView(R.id.scan_line)
+    ImageView scanLine;
+    @BindView(R.id.capture_crop_view)
+    RelativeLayout scanCropView;
+    @BindView(R.id.capture_container)
+    RelativeLayout scanContainer;
+
+    private final static String TAG = "CaptureAct";
+
+    private boolean isPause = false;
+    private Camera camera;
+    private InactivityTimer inactivityTimer;
+    private BeepManager beepManager;
+    private ObjectAnimator objectAnimator;
+    private boolean isHasSurface = false;
+
+    /**
+     * 扫码初始化
+     */
+    private void initScan() {
+        //扫描线动画1(属性动画可暂停)
+        float curTranslationY = scanLine.getTranslationY();
+        objectAnimator = ObjectAnimator.ofFloat(scanLine, "translationY", curTranslationY, UIUtil.dp2Px(this, 190f));
+        objectAnimator.setDuration(3000);
+        objectAnimator.setInterpolator(new LinearInterpolator());
+        objectAnimator.setRepeatCount(ObjectAnimator.INFINITE);
+        objectAnimator.setRepeatMode(ValueAnimator.RESTART);
+    }
+
+    /**
+     * 开始扫码
+     */
+    private void startScan() {
+        inactivityTimer = new InactivityTimer(this);
+        beepManager = new BeepManager(this);
+
+//        扫描线动画2(补间动画)
+//        translateAnimation = new TranslateAnimation(Animation.RELATIVE_TO_PARENT, 0.0f,
+//                Animation.RELATIVE_TO_PARENT, 0.0f, Animation.RELATIVE_TO_PARENT, 0.0f, Animation.RELATIVE_TO_PARENT, 0.9f);
+//        translateAnimation.setDuration(4500);
+//        translateAnimation.setRepeatCount(-1);
+//        translateAnimation.setRepeatMode(Animation.RESTART);
+//        scanLine.startAnimation(translateAnimation);
+
+
+        if (isPause) {
+            objectAnimator.resume();
+            isPause = false;
+        } else {
+            objectAnimator.start();
+        }
+
+        cameraManager = new CameraManager(getApplication());
+        handler = null;
+        if (isHasSurface) {
+            initCamera(scanPreview.getHolder());
+        } else {
+            scanPreview.getHolder().addCallback(this);
+        }
+        inactivityTimer.onResume();
+    }
+
+    /**
+     * 暂停扫码
+     */
+    private void pauseScan() {
+        if (handler != null) {
+            handler.quitSynchronously();
+            handler = null;
+        }
+        inactivityTimer.onPause();
+        beepManager.close();
+        cameraManager.closeDriver();
+        if (!isHasSurface) {
+            scanPreview.getHolder().removeCallback(this);
+        }
+        objectAnimator.pause();
+//        translateAnimation.cancel();
+        isPause = true;
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+        if (holder == null) {
+            Log.e(TAG, "*** WARNING *** surfaceCreated() gave us a null surface!");
+        }
+        if (!isHasSurface) {
+            isHasSurface = true;
+            initCamera(holder);
+        }
+    }
+
+    /**
+     * 扫码成功回调方法
+     * A valid barcode has been found, so give an indication of success and show
+     * the results.
+     *
+     * @param rawResult The contents of the barcode.
+     * @param bundle    The extras
+     */
+    @Override
+    public void handleDecode(Result rawResult, Bundle bundle) {
+        inactivityTimer.onActivity();
+        beepManager.playBeepSoundAndVibrate();
+        // rawResult.getText();
+    }
+
+    /**
+     * 初始化相机
+     *
+     * @param surfaceHolder
+     */
+    private void initCamera(SurfaceHolder surfaceHolder) {
+        if (surfaceHolder == null) {
+            throw new IllegalStateException("No SurfaceHolder provided");
+        }
+        if (cameraManager.isOpen()) {
+            Log.w(TAG, "initCamera() while already open -- late SurfaceView callback?");
+            return;
+        }
+        try {
+            cameraManager.openDriver(surfaceHolder);
+            if (handler == null) {
+                handler = new CaptureActivityHandler(this, cameraManager, DecodeThread.ALL_MODE);
+            }
+            initCrop();
+        } catch (IOException ioe) {
+            Log.w(TAG, ioe);
+            displayFrameworkBugMessageAndExit();
+        } catch (RuntimeException e) {
+            Log.w(TAG, "Unexpected error initializing camera", e);
+            displayFrameworkBugMessageAndExit();
+        }
+    }
+
+    /**
+     * 相机打开出错弹框
+     */
+    private void displayFrameworkBugMessageAndExit() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("相机打开出错，请稍后重试");
+        builder.setPositiveButton("确定", new DialogInterface.OnClickListener() {
+
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                finish();
+            }
+
+        });
+        builder.setOnCancelListener(new DialogInterface.OnCancelListener() {
+
+            @Override
+            public void onCancel(DialogInterface dialog) {
+                finish();
+            }
+        });
+        builder.show();
+    }
+
+    /**
+     * 初始化截取的矩形区域
+     */
+    private void initCrop() {
+        int cameraWidth = cameraManager.getCameraResolution().y;
+        int cameraHeight = cameraManager.getCameraResolution().x;
+
+        /** 获取布局中扫描框的位置信息 */
+        int[] location = new int[2];
+        scanCropView.getLocationInWindow(location);
+
+        int cropLeft = location[0];
+        int cropTop = location[1] - getStatusBarHeight();
+
+        int cropWidth = scanCropView.getWidth();
+        int cropHeight = scanCropView.getHeight();
+
+        /** 获取布局容器的宽高 */
+        int containerWidth = scanContainer.getWidth();
+        int containerHeight = scanContainer.getHeight();
+
+        /** 计算最终截取的矩形的左上角顶点x坐标 */
+        int x = cropLeft * cameraWidth / containerWidth;
+        /** 计算最终截取的矩形的左上角顶点y坐标 */
+        int y = cropTop * cameraHeight / containerHeight;
+
+        /** 计算最终截取的矩形的宽度 */
+        int width = cropWidth * cameraWidth / containerWidth;
+        /** 计算最终截取的矩形的高度 */
+        int height = cropHeight * cameraHeight / containerHeight;
+
+        /** 生成最终的截取的矩形 */
+        mCropRect = new Rect(x, y, width + x, height + y);
+    }
+
+    /**
+     * 获取状态栏高度
+     *
+     * @return
+     */
+    private int getStatusBarHeight() {
+        try {
+            Class<?> c = Class.forName("com.android.internal.R$dimen");
+            Object obj = c.newInstance();
+            Field field = c.getField("status_bar_height");
+            int x = Integer.parseInt(field.get(obj).toString());
+            return getResources().getDimensionPixelSize(x);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return 0;
+    }
+
+    //打开闪光灯
+    private void openFlashlight() {
+        camera = cameraManager.getCamera();
+        Camera.Parameters parameters = camera.getParameters();
+        parameters.setFlashMode(Camera.Parameters.FLASH_MODE_TORCH);
+        camera.setParameters(parameters);
+        camera.startPreview();
+    }
+
+    //关闭闪光灯
+    private void closeFlashlight() {
+        Camera.Parameters parameters = camera.getParameters();
+        parameters.setFlashMode(Camera.Parameters.FLASH_MODE_OFF);
+        camera.setParameters(parameters);
+        camera.startPreview();
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        isHasSurface = false;
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected int setLayoutId() {
+        return R.layout.activity_capture;
+    }
+}
---
+++ libs/src/main/java/com/jiaop/libs/zxing/CaptureAct.java
@@ -0,0 +1,49 @@
+package com.jiaop.libs.zxing;
+
+import android.graphics.Rect;
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.google.zxing.Result;
+import com.jiaop.libs.BaseActivity;
+
+/**
+ * <pre>
+ *     author : jiaop
+ *     time   : 2018/7/27
+ *     desc   : 二维码扫描封装展示页面，必须继承此类
+ *     version: 1.0.0
+ * </pre>
+ */
+public class CaptureAct extends BaseActivity {
+
+    protected CaptureActivityHandler handler;
+    protected CameraManager cameraManager;
+    protected Rect mCropRect = null;
+
+    public Handler getHandler() {
+        return handler;
+    }
+
+    public Rect getCropRect() {
+        return mCropRect;
+    }
+
+    public CameraManager getCameraManager() {
+        return cameraManager;
+    }
+
+    public void handleDecode(Result rawResult, Bundle bundle) {
+
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected int setLayoutId() {
+        return 0;
+    }
+}
